d9e1c0295f2a1cf4de43c89090261df7235dd0de
BISIAUX Alexandre <bisiaux.alexandre@gmail.com>
2012-01-25 21:43:42 +0100

    alex

--- src/gui.c
+++ src/gui.c
@@ -177,6 +177,13 @@ void Display_Init(SDisplay *display, SGame* game)
 	display->positions[27].x = 292;
 	display->positions[27].y = 220;
 	
+	// Chargement des actions de jeu
+	strcpy(temp_path, display->img_path);
+	strcat(temp_path, "double.png");
+	display->gameActions[0] = IMG_Load(temp_path);
+	strcpy(temp_path, display->img_path);
+	strcat(temp_path, "launch.png");
+	display->gameActions[1] = IMG_Load(temp_path);
 	
 	// Initialisation de la fenêtre
 	display->screen = SDL_SetVideoMode(display->window_width, display->window_height, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
@@ -849,7 +856,7 @@ int allInJan( SGameState* gameState, EPlayer player)
 		return 1;
 	}
 }
-				
+
 int Display_CheckersPossibilities(SDisplay *display, SGameState *gameState, EPlayer player, SGame *game, int Die_For_Play[7])
 {
 	int i=0;
@@ -973,7 +980,6 @@ int Display_CheckersPossibilities(SDisplay *display, SGameState *gameState, EPla
 	return 0;
 }
 
-
 int* Display_Arrow_Possibilities(SDisplay *display, SGameState *gameState, EPlayer player, EPosition depart, SGame *game)
 {
 	Display_RefreshGameBoard(display, gameState, game);
@@ -1086,4 +1092,230 @@ int inTab(EPosition p,int tab[28])
 	}
 	return 0;
 }
- 
+
+
+int Display_GameActions(SDisplay *display, SGameState* gameState, SGame *game)
+{
+	SDL_Rect pos;
+	int notSelected = 1;
+	SDL_Surface *erase = SDL_CreateRGBSurface(SDL_HWSURFACE, 160, 45, 32, 0, 0, 0, 0);
+	pos.x = 630;
+	pos.y = 190;
+	if(game->withDouble)
+	{
+		SDL_BlitSurface(display->gameActions[0], NULL, display->screen,&pos);
+		pos.y = 250;
+	}
+	SDL_BlitSurface(display->gameActions[1], NULL, display->screen,&pos);
+	
+	SDL_Flip(display->screen);
+	SDL_Event event;
+	while(1)
+	{
+		SDL_WaitEvent(&event);
+		switch(event.type)
+		{
+			case SDL_QUIT:
+				return 1;
+				break;
+			case SDL_MOUSEBUTTONUP:
+				if (event.button.button == SDL_BUTTON_LEFT)
+				{
+					if((event.button.x>=630 && event.button.x<=790 && event.button.y>=190 && event.button.y<=235) && game->withDouble && notSelected) // Double
+					{
+						game->doubleValue = game->doubleValue * 2;
+						pos.x = 630;
+						pos.y = 190;
+						SDL_BlitSurface(erase, NULL, display->screen,&pos);
+						notSelected = 1;
+					}
+					if((event.button.x>=630 && event.button.x<=790 && event.button.y>=190 && event.button.y<=235) && game->withDouble) // Lancer
+					{
+						Game_LaunchDie(gameState, game);
+						pos.x = 630;
+						pos.y = 250;
+						SDL_BlitSurface(erase, NULL, display->screen,&pos);
+					
+						return 0;
+					}
+				}
+		  		break;
+
+		}
+	}
+}
+/* MARCHE PAS DU TOUT
+int Display_CheckersPossibilities(SDisplay *display, SGameState *gameState, EPlayer player, SGame *game, int Die_For_Play[7])
+{
+	
+	
+	int mat[28][28];
+	int i,j,quit=0;
+	
+	int noneMove = 1;
+	
+	// Initialisation de la matrice de stockage des couples de mouvement possibles (src,dest)
+	for(i=0;i<28;i++)
+	{
+		for(j=0;j<28;j++)
+		{
+			mat[i][j] = 0;
+		}
+	}
+	
+	int sens =1;
+	if(player == EPlayer2) sens = -1;
+	SMove tmp;
+	
+	// Remplissage de la matrice
+	for(i=0;i<28;i++)
+	{
+		if(gameState->zones[i].player == player && gameState->zones[i].nb_checkers>0) // Si les pions sur la flèche appartiennent au joueur
+		{
+			// Alors on regarde les destinations possibles et on stocke les couples possibles dans la matrice en inscrivant 1 dans la case correspondante
+
+			tmp.src_point = (EPosition)i;
+			tmp.dest_point = (EPosition)i+sens*gameState->die1;
+			if(authorized_deplacement(gameState,&tmp, player, Die_For_Play)) // Si le déplacement flèche est autorisé
+			{
+				mat[i][i+sens*gameState->die1] = 1;
+				printf("MOUVEMENT %d => %d POSSIBLE\n",tmp.src_point,tmp.dest_point);
+				noneMove = 0;
+			}
+			tmp.dest_point = (EPosition)i+sens*gameState->die2;
+			if(authorized_deplacement(gameState,&tmp, player, Die_For_Play)) // Si le déplacement flèche est autorisé
+			{
+				mat[i][i+sens*gameState->die1] = 1;
+				printf("MOUVEMENT %d => %d POSSIBLE\n",tmp.src_point,tmp.dest_point);
+				noneMove = 0;
+			}
+			tmp.dest_point = (EPosition)i+sens*(gameState->die1+gameState->die2);
+			if(authorized_deplacement(gameState,&tmp, player, Die_For_Play)) // Si le déplacement flèche est autorisé
+			{
+				mat[i][i+sens*gameState->die1] = 1;
+				printf("MOUVEMENT %d => %d POSSIBLE\n",tmp.src_point,tmp.dest_point);
+				noneMove = 0;
+			}
+		}
+	}
+	
+	
+	if( noneMove) // Si pas de mouvement possible on affiche un message
+	{
+		SDL_Rect msg_position;
+		msg_position.x = 80;
+		msg_position.y = 325;
+		SDL_Color msg_color = {255, 255, 255, 0};
+		Display_Message( display, "Pas de mouvement possible!", msg_position, msg_color,1);
+		SDL_Flip(display->screen);
+		SDL_Delay(1000);
+	}
+	else // Sinon On appelle la fonction pour choisir le mouvement
+	{
+		quit = makeChoice(display, gameState, game, mat, player);
+	}
+	return quit;
+}
+
+int src_valid(int mat[28][28], EPosition src)
+{
+	int i,j;
+	for(i=0;i<28;i++)
+	{
+		for(j=0;j<28;j++)
+		{
+			if(mat[i][j])
+				return 1;
+		}
+	}
+	return 0;
+}
+
+int dest_valid(int mat[28][28], EPosition src, EPosition dest)
+{
+	return mat[src][dest];
+}
+
+int nbDesRestant(SGame* game)
+{
+	return 0;
+}
+int makeChoice(SDisplay *display, SGameState *gameState, SGame *game, int mat[28][28], EPlayer player)
+{
+	SDL_Event event;
+	int posDepartSelected = 0;
+	
+	// Affichage des pions qui peuvent être bougés
+	int i,j;
+	for(i=0;i<28;i++)
+	{
+		for(j=0;j<28;j++)
+		{
+			if(mat[i][j])
+				colorChecker(display,gameState,i);
+		}
+	}
+	
+	SDL_Flip(display->screen);
+	
+	while(1)
+	{
+		SDL_WaitEvent(&event);
+		EPosition posDepart;
+		EPosition posArrivee;
+		int aut =0;
+		SMove mouvement;
+		switch(event.type)
+		{
+			case SDL_QUIT:
+				return 1;
+				
+			case SDL_MOUSEBUTTONUP:
+				if (event.button.button == SDL_BUTTON_LEFT)
+				{
+					//Si il y a clic sur un autre pion de départ cliqué, malgré qu'un pion de départ ait déja été choisi avant
+					if( posDepartSelected && CheckerWithScreenPosition(event.button.x, event.button.y, &posDepart) && src_valid(mat,posDepart))
+					{
+						posDepartSelected = !posDepartSelected;
+					}
+					
+					if(!posDepartSelected) // selection de la position depart
+					{
+						if(CheckerWithScreenPosition(event.button.x, event.button.y, &posDepart)) // il a trouvé une position valide 
+						{
+							if(src_valid(mat,posDepart))
+							{
+								Display_Arrow_Possibilities(display, gameState, player, posDepart, game);
+								posDepartSelected = 1;
+								mouvement.src_point = posDepart+1;
+							}
+						}
+					}
+					else // selection de la position arrivée
+					{
+						if(CheckerWithScreenPosition(event.button.x, event.button.y, &posArrivee)) // il a trouvé une position valide
+						{
+							printf("POS ARRIVEE\n");
+							if(dest_valid(mat, posDepart, posArrivee))
+							{
+								printf("POS ARRIVEE VALIDE\n");
+								mouvement.dest_point = posArrivee+1;
+
+								printf("JOUEUR %d\tSRC %d\tDEST %d\tDIE1 %d\tDIE2 %d MVT AUTORISE %d\n",player,mouvement.src_point,mouvement.dest_point,gameState->die1,gameState->die2,aut);
+								
+								Checker_Move(display,gameState,&mouvement, game);
+								return 0;
+							}
+						}
+						else // Si clic ailleur on efface les possibilités et on réaffiche les pions déplaçables.
+						{
+							Display_RefreshGameBoard(display, gameState, game);
+							return makeChoice(display, gameState, game, mat, player);
+						}
+					}
+				}
+				break;
+		}
+	}
+}
+*/
