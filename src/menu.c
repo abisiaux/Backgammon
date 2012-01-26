7fcef93b0223df46ea157e68e2068ff65acdc3a9
BISIAUX Alexandre <bisiaux.alexandre@gmail.com>
2012-01-26 19:09:07 +0100

    Ajout de la doc

--- src/menu.c
+++ src/menu.c
@@ -1,7 +1,14 @@
+/**
+ * \file menu.c
+ * \brief Contient l'implémentation des fonctions d'affichage du menu
+ * \author Alexandre BISIAUX et Antonin BIRET
+ * \date 26 janvier 2012
+ *
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <time.h>
 #include <unistd.h>
 
 #include <SDL/SDL.h>
@@ -13,42 +20,42 @@
 
 int Menu_Click(int x, int y)
 {
-	if ( x>=260 && x<=300 && y>=284 && y<=315 ) // Jeton vert choisi
+	if (x>=260 && x<=300 && y>=284 && y<=315) // Jeton vert choisi
 		return 1;
 		
-	if ( x>=402 && x<=444 && y>=284 && y<=315 ) // Jeton blanc choisi
+	if (x>=402 && x<=444 && y>=284 && y<=315) // Jeton blanc choisi
 		return 2;
 		
-	if ( x>=260 && x<=300 && y>=330 && y<=360 ) // Jouer avec le double
+	if (x>=260 && x<=300 && y>=330 && y<=360) // Jouer avec le double
 		return 3;
 		
-	if ( x>=402 && x<=444 && y>=330 && y<=360 ) // Jouer sans le double
+	if (x>=402 && x<=444 && y>=330 && y<=360) // Jouer sans le double
 		return 4;
 
-	if ( x>=64 && x<=210 && y>=442 && y<=480 ) // Quitter la partie
+	if (x>=64 && x<=210 && y>=442 && y<=480) // Quitter la partie
 		return 5;
 	
-	if ( x>=295 && x<=412 && y>=442 && y<=480 ) // Jouer
+	if (x>=295 && x<=412 && y>=442 && y<=480) // Jouer
 		return 6;
 	
-	if( x>=262 && x<=532 && y>=207 && y<=244 ) // Clic sur changer nom Joueur 1
+	if(x>=262 && x<=532 && y>=207 && y<=244) // Clic sur changer nom Joueur 1
 		return 7;
 		
-	if( x>=262 && x<=532 && y>=260 && y<=297 ) // Clic sur changer nom Joueur 2
+	if(x>=262 && x<=532 && y>=260 && y<=297) // Clic sur changer nom Joueur 2
 		return 8;
 
-	if( x>=273 && x<=300 && y>=379 && y<=411 ) // Clic sur flèche score gauche
+	if(x>=273 && x<=300 && y>=379 && y<=411) // Clic sur flèche score gauche
 		return 9;
 	
-	if( x>=357 && x<=386 && y>=379 && y<=411 ) // Clic sur flèche score gauche
+	if(x>=357 && x<=386 && y>=379 && y<=411) // Clic sur flèche score gauche
 		return 10;
 	
 	return -1; // Clic ailleur
 }
 
-void Menu_Text(SDisplay	*display, char	*message, SDL_Rect position, SDL_Color color)
+void Menu_Text(SDisplay* display, char* message, SDL_Rect position, SDL_Color color)
 {
-	SDL_Surface *msg = TTF_RenderText_Blended(display->font,message,color);
+	SDL_Surface *msg = TTF_RenderText_Blended(display->font, message,color);
 	SDL_BlitSurface(msg, NULL, display->screen, &position);
 	SDL_FreeSurface(msg);
 	SDL_Flip(display->screen);
@@ -68,12 +75,9 @@ void Menu_TextInput(char* name, SDL_keysym key)
     {
         int ascii = -1;
 
-        if ((key.sym >= SDLK_a && key.sym <= SDLK_z) ||
-            (key.sym >= SDLK_0 && key.sym <= SDLK_9) ||
-            key.sym == SDLK_SPACE)
+        if ((key.sym >= SDLK_a && key.sym <= SDLK_z) || (key.sym >= SDLK_0 && key.sym <= SDLK_9) || key.sym == SDLK_SPACE)
         {
             ascii = key.sym;
-
             // Gestion de la majuscule
             if (((key.mod & KMOD_SHIFT) && (key.sym >= SDLK_a && key.sym <= SDLK_z)) || ((key.mod & KMOD_CAPS) && (key.sym >= SDLK_a && key.sym <= SDLK_z)))
                 ascii -= 32;
@@ -89,7 +93,7 @@ void Menu_TextInput(char* name, SDL_keysym key)
    }
 }
 
-void Menu_Fill(SDisplay *display,EGameMode gameMode, SGame* game)
+void Menu_Fill(SDisplay* display, EGameMode gameMode, SGame* game)
 {
 	SDL_Rect menu_position;
 	SDL_Rect check_position;
@@ -135,7 +139,7 @@ void Menu_Fill(SDisplay *display,EGameMode gameMode, SGame* game)
 		// Affichage du nom du joueur
 		Menu_Text(display,game->player1_name,player1_name_position,c);
 		
-		// Affichage du score
+		// Affichage du choix de la limite de score
 		check_position.x = 320;
 		check_position.y = 375;
 		Menu_Text(display, scoreLimit, check_position,col);
@@ -176,7 +180,7 @@ void Menu_Fill(SDisplay *display,EGameMode gameMode, SGame* game)
 		Menu_Text(display,game->player1_name,player1_name_position,c);
 		Menu_Text(display,game->player2_name,player2_name_position,c);
 		
-		// Affichage du score
+		// Affichage du choix de la limite de score
 		check_position.x = 320;
 		check_position.y = 378;
 		Menu_Text(display, scoreLimit, check_position,col);
@@ -199,17 +203,16 @@ void Menu_Fill(SDisplay *display,EGameMode gameMode, SGame* game)
 	
 	free(temp_path);
 	free(scoreLimit);
-	//free(background); // Invalid Free ????
 }
 	
-int Menu_Display(SDisplay *display, EGameMode gameMode, SGame* game)
+int Menu_Display(SDisplay* display, EGameMode gameMode, SGame* game)
 {
 	SDL_Color col = {255, 255, 255, 0};
 	SDL_Surface *player_name_selection;
 	int numPlayerSelected = 0;
 	SDL_Rect temp_position;
 	
-	char *temp_path;
+	char* temp_path;
 	
 	temp_path = (char*)malloc(100*sizeof(char));
 	strcpy(temp_path, display->img_path);
@@ -223,17 +226,17 @@ int Menu_Display(SDisplay *display, EGameMode gameMode, SGame* game)
 	SDL_Flip(display->screen);
 	while(1)
 	{
-	
 		SDL_WaitEvent(&event);
 	
 		switch(event.type)
 		{
 			case SDL_QUIT:
 				return -1;
+				
 			case SDL_MOUSEBUTTONUP:
 				if (event.button.button == SDL_BUTTON_LEFT)
 				{
-					switch( Menu_Click(event.button.x, event.button.y) )
+					switch(Menu_Click(event.button.x, event.button.y))
 					{
 						case 1 : // Clic sur le jeton vert
 							if(gameMode == HUMAN_IA)
@@ -273,9 +276,9 @@ int Menu_Display(SDisplay *display, EGameMode gameMode, SGame* game)
 						case 6 : // Clic sur jouer
 							temp_position.x = 55;
 							temp_position.y = 300;
-							if( gameMode == HUMAN_HUMAN )
+							if(gameMode == HUMAN_HUMAN)
 							{
-								if( strcmp(game->player1_name,"") && strcmp(game->player2_name,""))
+								if(strcmp(game->player1_name,"") && strcmp(game->player2_name,""))
 								{
 									free(temp_path);
 									return 0;
@@ -283,7 +286,7 @@ int Menu_Display(SDisplay *display, EGameMode gameMode, SGame* game)
 							}
 							else
 							{
-								if( strcmp(game->player1_name,"") )
+								if(strcmp(game->player1_name,""))
 								{
 									free(temp_path);
 									return 0;
@@ -338,7 +341,7 @@ int Menu_Display(SDisplay *display, EGameMode gameMode, SGame* game)
 				}
 				break;
 			case SDL_KEYDOWN:
-				if(numPlayerSelected == 1 )
+				if(numPlayerSelected == 1)
 				{
 					if(event.key.keysym.sym == SDLK_TAB && gameMode != HUMAN_IA)
 					{
